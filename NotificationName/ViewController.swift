006576b6fae522c39cd31d4e6438b1517cc44282
ericyu423 <ericyu423@hotmail.com>
2017-11-11 20:50:24 -0800

    short example

--- NotificationName/ViewController.swift
+++ NotificationName/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
+extension Notification.Name {
+    
+    static let MyCustomNotification = Notification.Name("MyCustom")
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segControl: UISegmentedControl!
+    @IBOutlet weak var mylabel: UILabel!
+    @IBOutlet weak var textfield: UITextField!
+    @IBAction func changeLabelButton(_ sender: UIButton) {
+       
+    }
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange(_:)), name: Notification.Name.UITextFieldTextDidChange, object: textfield)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(textStartEdit(_:)), name: Notification.Name.UITextFieldTextDidBeginEditing, object: textfield)
+        //if you don't put in the object nothing will happen
+  
+        NotificationCenter.default.addObserver(self, selector: #selector(seqControl(_:)), name: Notification.Name.MyCustomNotification, object: nil)
+  
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func SegmentControlDidChange(_ sender: UISegmentedControl) {
+        
+        NotificationCenter.default.post(name: Notification.Name.MyCustomNotification, object: nil)
     }
+    
+    @objc func seqControl(_ notification: Notification){
+        mylabel.text = "got here with custom notification"
+        
+    }
+    @objc func textDidChange(_ notification: Notification){
+        
+        mylabel.text =  "text did change bring me here"
 
-
+    }
+    
+    @objc func textStartEdit(_ notification: Notification){
+        mylabel.text =  "text become First Responder"
+        
+    }
 }
 
